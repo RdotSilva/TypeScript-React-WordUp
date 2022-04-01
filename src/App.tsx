d3a76feff2b2371ad103712c90c125dd5d5d01ec
RdotSilva <RyandotSilva@gmail.com>
2022-04-01 21:02:38 +0100

    Add board util

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Board />
+      <Keyboard />
     </div>
   );
 }
