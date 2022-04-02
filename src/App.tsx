7cbaf7537e35054b886f1de14de8cc8c270d6863
RdotSilva <RyandotSilva@gmail.com>
2022-04-02 21:21:23 +0100

    Add AppContext for board

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./utils/board";
+
+export const AppContext = createContext({});
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
-      <Board />
-      <Keyboard />
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
