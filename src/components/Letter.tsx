7d41792a0e2f625f1182cdddfb94bb0589e49ee4
RdotSilva <RyandotSilva@gmail.com>
2022-04-02 21:05:43 +0100

    Add Letter component

---
+++ src/components/Letter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {
+  letterPos: string;
+  attemptVal: string;
+};
+
+const Letter = ({ letterPos, attemptVal }: Props) => {
+  return <div>Letter</div>;
+};
+
+export default Letter;
