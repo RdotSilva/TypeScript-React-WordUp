c11d4fb62ff7f129e231f5ef4fa90c9edee9b7f9
RdotSilva <RyandotSilva@gmail.com>
2022-04-02 21:02:34 +0100

    Add placeholder rows

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { boardDefault } from "../utils/board";
 
 type Props = {};
 
 const Board = (props: Props) => {
-  return <div>Board</div>;
+  const [board, setBoard] = useState(boardDefault);
+  return (
+    <div>
+      <div>Row 1</div>
+      <div>Row 2</div>
+      <div>Row 3</div>
+      <div>Row 4</div>
+      <div>Row 5</div>
+      <div>Row 6</div>
+    </div>
+  );
 };
 
 export default Board;
