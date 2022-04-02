eb96b5010f17f094ad28507d651f34ae87993fde
RdotSilva <RyandotSilva@gmail.com>
2022-04-02 21:10:21 +0100

    Replace placeholder rows with letters

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { boardDefault } from "../utils/board";
+import Letter from "./Letter";
 
 type Props = {};
 
@@ -7,12 +8,48 @@ const Board = (props: Props) => {
   const [board, setBoard] = useState(boardDefault);
   return (
     <div>
-      <div>Row 1</div>
-      <div>Row 2</div>
-      <div>Row 3</div>
-      <div>Row 4</div>
-      <div>Row 5</div>
-      <div>Row 6</div>
+      <div>
+        <Letter letterPos={0} attemptVal={0} />
+        <Letter letterPos={1} attemptVal={0} />
+        <Letter letterPos={2} attemptVal={0} />
+        <Letter letterPos={3} attemptVal={0} />
+        <Letter letterPos={4} attemptVal={0} />
+      </div>
+      <div>
+        <Letter letterPos={0} attemptVal={1} />
+        <Letter letterPos={1} attemptVal={1} />
+        <Letter letterPos={2} attemptVal={1} />
+        <Letter letterPos={3} attemptVal={1} />
+        <Letter letterPos={4} attemptVal={1} />
+      </div>
+      <div>
+        <Letter letterPos={0} attemptVal={2} />
+        <Letter letterPos={1} attemptVal={2} />
+        <Letter letterPos={2} attemptVal={2} />
+        <Letter letterPos={3} attemptVal={2} />
+        <Letter letterPos={4} attemptVal={2} />
+      </div>
+      <div>
+        <Letter letterPos={0} attemptVal={3} />
+        <Letter letterPos={1} attemptVal={3} />
+        <Letter letterPos={2} attemptVal={3} />
+        <Letter letterPos={3} attemptVal={3} />
+        <Letter letterPos={4} attemptVal={3} />
+      </div>
+      <div>
+        <Letter letterPos={0} attemptVal={4} />
+        <Letter letterPos={1} attemptVal={4} />
+        <Letter letterPos={2} attemptVal={4} />
+        <Letter letterPos={3} attemptVal={4} />
+        <Letter letterPos={4} attemptVal={4} />
+      </div>
+      <div>
+        <Letter letterPos={0} attemptVal={5} />
+        <Letter letterPos={1} attemptVal={5} />
+        <Letter letterPos={2} attemptVal={5} />
+        <Letter letterPos={3} attemptVal={5} />
+        <Letter letterPos={4} attemptVal={5} />
+      </div>
     </div>
   );
 };
