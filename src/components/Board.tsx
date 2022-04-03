b769f5ae8c4edeabcacf86317c3e339dcef97642
RdotSilva <RyandotSilva@gmail.com>
2022-04-03 14:49:51 +0100

    Update board styling

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,48 +5,50 @@ type Props = {};
 
 const Board = (props: Props) => {
   return (
-    <div>
-      <div>
-        <Letter letterPos={0} attemptVal={0} />
-        <Letter letterPos={1} attemptVal={0} />
-        <Letter letterPos={2} attemptVal={0} />
-        <Letter letterPos={3} attemptVal={0} />
-        <Letter letterPos={4} attemptVal={0} />
-      </div>
-      <div>
-        <Letter letterPos={0} attemptVal={1} />
-        <Letter letterPos={1} attemptVal={1} />
-        <Letter letterPos={2} attemptVal={1} />
-        <Letter letterPos={3} attemptVal={1} />
-        <Letter letterPos={4} attemptVal={1} />
-      </div>
-      <div>
-        <Letter letterPos={0} attemptVal={2} />
-        <Letter letterPos={1} attemptVal={2} />
-        <Letter letterPos={2} attemptVal={2} />
-        <Letter letterPos={3} attemptVal={2} />
-        <Letter letterPos={4} attemptVal={2} />
-      </div>
-      <div>
-        <Letter letterPos={0} attemptVal={3} />
-        <Letter letterPos={1} attemptVal={3} />
-        <Letter letterPos={2} attemptVal={3} />
-        <Letter letterPos={3} attemptVal={3} />
-        <Letter letterPos={4} attemptVal={3} />
-      </div>
-      <div>
-        <Letter letterPos={0} attemptVal={4} />
-        <Letter letterPos={1} attemptVal={4} />
-        <Letter letterPos={2} attemptVal={4} />
-        <Letter letterPos={3} attemptVal={4} />
-        <Letter letterPos={4} attemptVal={4} />
-      </div>
-      <div>
-        <Letter letterPos={0} attemptVal={5} />
-        <Letter letterPos={1} attemptVal={5} />
-        <Letter letterPos={2} attemptVal={5} />
-        <Letter letterPos={3} attemptVal={5} />
-        <Letter letterPos={4} attemptVal={5} />
+    <div className="flex justify-center">
+      <div className="grid grid-rows-5 gap-1 w-24 ">
+        <div className="grid grid-cols-5 gap-1">
+          <Letter letterPos={0} attemptVal={0} />
+          <Letter letterPos={1} attemptVal={0} />
+          <Letter letterPos={2} attemptVal={0} />
+          <Letter letterPos={3} attemptVal={0} />
+          <Letter letterPos={4} attemptVal={0} />
+        </div>
+        <div className="grid grid-cols-5 gap-1">
+          <Letter letterPos={0} attemptVal={1} />
+          <Letter letterPos={1} attemptVal={1} />
+          <Letter letterPos={2} attemptVal={1} />
+          <Letter letterPos={3} attemptVal={1} />
+          <Letter letterPos={4} attemptVal={1} />
+        </div>
+        <div className="grid grid-cols-5 gap-1">
+          <Letter letterPos={0} attemptVal={2} />
+          <Letter letterPos={1} attemptVal={2} />
+          <Letter letterPos={2} attemptVal={2} />
+          <Letter letterPos={3} attemptVal={2} />
+          <Letter letterPos={4} attemptVal={2} />
+        </div>
+        <div className="grid grid-cols-5 gap-1">
+          <Letter letterPos={0} attemptVal={3} />
+          <Letter letterPos={1} attemptVal={3} />
+          <Letter letterPos={2} attemptVal={3} />
+          <Letter letterPos={3} attemptVal={3} />
+          <Letter letterPos={4} attemptVal={3} />
+        </div>
+        <div className="grid grid-cols-5 gap-1">
+          <Letter letterPos={0} attemptVal={4} />
+          <Letter letterPos={1} attemptVal={4} />
+          <Letter letterPos={2} attemptVal={4} />
+          <Letter letterPos={3} attemptVal={4} />
+          <Letter letterPos={4} attemptVal={4} />
+        </div>
+        <div className="grid grid-cols-5 gap-1">
+          <Letter letterPos={0} attemptVal={5} />
+          <Letter letterPos={1} attemptVal={5} />
+          <Letter letterPos={2} attemptVal={5} />
+          <Letter letterPos={3} attemptVal={5} />
+          <Letter letterPos={4} attemptVal={5} />
+        </div>
       </div>
     </div>
   );
