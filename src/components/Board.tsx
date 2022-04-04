5776df8db00ebe3210e4bd6bd7a53f1e0ff41f94
RdotSilva <RyandotSilva@gmail.com>
2022-04-04 20:43:33 +0100

    Revert to grid to line up rows

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,43 +6,43 @@ type Props = {};
 const Board = (props: Props) => {
   return (
     <div className="flex justify-center">
-      <div className="grid grid-rows-5 gap-1 w-24 ">
-        <div className="flex gap-2">
+      <div className="grid grid-rows-5 gap-2 w-70">
+        <div className="grid grid-cols-5 gap-1">
           <Letter letterPos={0} attemptVal={0} />
           <Letter letterPos={1} attemptVal={0} />
           <Letter letterPos={2} attemptVal={0} />
           <Letter letterPos={3} attemptVal={0} />
           <Letter letterPos={4} attemptVal={0} />
         </div>
-        <div className="flex gap-2">
+        <div className="grid grid-cols-5 gap-1">
           <Letter letterPos={0} attemptVal={1} />
           <Letter letterPos={1} attemptVal={1} />
           <Letter letterPos={2} attemptVal={1} />
           <Letter letterPos={3} attemptVal={1} />
           <Letter letterPos={4} attemptVal={1} />
         </div>
-        <div className="flex gap-2">
+        <div className="grid grid-cols-5 gap-1">
           <Letter letterPos={0} attemptVal={2} />
           <Letter letterPos={1} attemptVal={2} />
           <Letter letterPos={2} attemptVal={2} />
           <Letter letterPos={3} attemptVal={2} />
           <Letter letterPos={4} attemptVal={2} />
         </div>
-        <div className="flex gap-2">
+        <div className="grid grid-cols-5 gap-1">
           <Letter letterPos={0} attemptVal={3} />
           <Letter letterPos={1} attemptVal={3} />
           <Letter letterPos={2} attemptVal={3} />
           <Letter letterPos={3} attemptVal={3} />
           <Letter letterPos={4} attemptVal={3} />
         </div>
-        <div className="flex gap-2">
+        <div className="grid grid-cols-5 gap-1">
           <Letter letterPos={0} attemptVal={4} />
           <Letter letterPos={1} attemptVal={4} />
           <Letter letterPos={2} attemptVal={4} />
           <Letter letterPos={3} attemptVal={4} />
           <Letter letterPos={4} attemptVal={4} />
         </div>
-        <div className="flex gap-2">
+        <div className="grid grid-cols-5 gap-1">
           <Letter letterPos={0} attemptVal={5} />
           <Letter letterPos={1} attemptVal={5} />
           <Letter letterPos={2} attemptVal={5} />
