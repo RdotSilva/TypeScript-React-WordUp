720e00920a6ed2437822ebe89154beb043ece608
RdotSilva <RyandotSilva@gmail.com>
2022-04-05 19:15:44 +0100

    Add initial keyboard setup

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -3,7 +3,29 @@ import React from "react";
 type Props = {};
 
 const Keyboard = (props: Props) => {
-  return <div>Keyboard</div>;
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  return (
+    <div className="">
+      <div className="">
+        {keys1.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
+      <div className="">
+        {keys2.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
+      <div className="">
+        {keys3.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Keyboard;
