768152767cc9490bc96c6f5b7cb8fbcaa87d6e16
RdotSilva <RyandotSilva@gmail.com>
2022-04-06 18:17:51 +0100

    Update styles for keyboard

--- src/components/Keyboard/Key.tsx
+++ src/components/Keyboard/Key.tsx
@@ -6,7 +6,11 @@ type Props = {
 };
 
 const Key = ({ keyVal, bigKey }: Props) => {
-  return <div className={bigKey ? "w-10" : ""}>{keyVal}</div>;
+  return (
+    <div className={`border-solid border-2 ${bigKey ? "w-10" : ""}`}>
+      {keyVal}
+    </div>
+  );
 };
 
 export default Key;
