366994168017a7983e2290f806d7ff9a4362b0ba
RdotSilva <RyandotSilva@gmail.com>
2022-04-07 21:23:37 +0100

    Add styles for Key component

--- src/components/Keyboard/Key.tsx
+++ src/components/Keyboard/Key.tsx
@@ -5,9 +5,17 @@ type Props = {
   bigKey?: boolean;
 };
 
+const styles = {
+  width: `40px`,
+  height: "58px",
+};
+
 const Key = ({ keyVal, bigKey }: Props) => {
   return (
-    <div className={`border-solid border-2 ${bigKey ? "w-10" : ""}`}>
+    <div
+      style={styles}
+      className={`flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none`}
+    >
       {keyVal}
     </div>
   );
